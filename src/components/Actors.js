b38dc93c279731cc19c6b9fb61a4220c17d0992a
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-07-21 17:18:50 +0300

    react Router Lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorsList=actors.map((actor)=>(
+    <div key={actor.name}>
+      <h1>{actor.name}</h1>
+      <ul>{actor.movies.map((actorsMovie)=>(
+        <li key={actorsMovie}>{actorsMovie}</li>
+      ))}</ul>
+    </div>
+  ))
+  return <div>
+    <h1>Actors Page</h1>
+    {actorsList}
+  </div>;
 }
 
 export default Actors;
